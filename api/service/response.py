da225c29860068944c63a49acd9a588d21334dc8
Mahasvan <mahasvan@gmail.com>
2025-04-24 22:55:26 +0530

    add response formatter and shell service

---
+++ api/service/response.py
@@ -0,0 +1,16 @@
+from fastapi.responses import JSONResponse
+
+class BetterJSONResponse(JSONResponse):
+    def __init__(self, content=None, status_code=200, headers=None, error=None):
+
+        if content:
+            content = {
+                "response": content
+            }
+        else:
+            content = {}
+
+        if error:
+            content["error"] = str(error)
+
+        super().__init__(content, status_code, headers)
