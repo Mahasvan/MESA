8bc20e73cecb45b0d2c1d647f59696dbf88d96b6
Mahasvan <mahasvan@gmail.com>
2025-10-31 10:57:43 +0530

    train cnn with reduced model complexity

--- dev/early_stopper.py
+++ dev/early_stopper.py
@@ -1,14 +1,10 @@
 class EarlyStopping:
+    """
+    This class is designed to record loss values, exit the training loop if the validation loss does not reduce for N continuous epochs.
+    """
     def __init__(self):
         self.history = []
 
-    def record_loss(self, value):
-        """
-        Records the loss for one epoch. this is added to the internal history, to decide early stopping
-        :param value:
-        :return: None
-        """
-        self.history.append(value)
 
     def _get_derivative(self):
         d = []
@@ -16,11 +12,13 @@ class EarlyStopping:
             d.append(self.history[i] - self.history[i-1])
         return d
 
-    def stop(self, window=3):
+    def record(self, value, window=3):
         """
+        :param value: the loss vaue to be recorded
         :param window: the number of past loss values to consider
         :return: True if early stopping, False otherwise
         """
+        self.history.append(value)
         if len(self.history)  < window+1: return False
         derivative = self._get_derivative()[-window:]
         # print(derivative)
