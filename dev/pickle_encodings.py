19275d739c64a2cc90fb48311db38fd55041b430
Mahasvan <mahasvan@gmail.com>
2025-11-27 11:11:39 +0530

    add logging to pickling

--- dev/pickle_encodings.py
+++ dev/pickle_encodings.py
@@ -1,3 +1,4 @@
+print("Importing libraries...")
 import os
 import pickle
 
@@ -6,21 +7,27 @@ import torch
 from sklearn.model_selection import train_test_split
 from transformers import AutoTokenizer
 
+print("Imported libraries!")
+
 maximum_length = 256  # Max length of input sequences
 
 threshold = 0.5
 DATASET_SIZE = 1_000
 
+print("Dataset size:", DATASET_SIZE)
+
 PICKLE_LOCATION = "../pickles"
 
 # Load and preprocess the dataset
 # Make sure the path to your CSV is correct
+print("Reading dataset...")
 df = pd.read_csv("../jigsaw/dataset_text_target.csv")
 df_true = df[df.target > 0.5]
 df_false = df[df.target <= 0.5]
 df = pd.concat([df_true[:DATASET_SIZE // 2], df_false[:DATASET_SIZE // 2]], axis=0)
 mapper = lambda x: 1 if x > 0.5 else 0
 df['target'] = df['target'].apply(mapper)
+print("Read dataset!")
 
 os.makedirs(PICKLE_LOCATION, exist_ok=True)
 
@@ -28,20 +35,27 @@ os.makedirs(PICKLE_LOCATION, exist_ok=True)
 x_train, x_test, y_train, y_test = train_test_split(
     df.comment_text, df.target, test_size=0.2, random_state=42, shuffle=True
 )
-
+print("Split dataset into train and test!")
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "mps" if (torch.mps and torch.mps.is_available()) else "cpu")
-print("DEVICE:", DEVICE)
+print("Using device:", DEVICE)
 
 
 model_name = "microsoft/deberta-v3-base"
 
 
 # Tokenize and encode the data using the BERT tokenizer
+print("Loading tokenizer:", model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name, do_lower_case=True)
+print("Loaded tokenizer!")
 
 train_file = os.path.join(PICKLE_LOCATION, "X_train_encoded.pkl")
 test_file = os.path.join(PICKLE_LOCATION, "X_test_encoded.pkl")
+
+print("Train file:", train_file)
+print("Test file:", test_file)
+
+print("Encoding train...")
 X_train_encoded = tokenizer.batch_encode_plus(
     x_train.tolist(),
     padding='max_length',
@@ -49,8 +63,16 @@ X_train_encoded = tokenizer.batch_encode_plus(
     max_length=maximum_length,
     add_special_tokens=True,
     return_tensors='pt',  # Return PyTorch tensors
+    verbose=True
 )
+print("Encoded train file!")
 
+print("Writing to disk...")
+with open(train_file, "wb") as f:
+    pickle.dump(X_train_encoded, f)
+print("Written to disk!")
+
+print("Encoding test...")
 X_test_encoded = tokenizer.batch_encode_plus(
     x_test.tolist(),
     padding='max_length',
@@ -58,11 +80,11 @@ X_test_encoded = tokenizer.batch_encode_plus(
     max_length=maximum_length,
     add_special_tokens=True,
     return_tensors='pt',  # Return PyTorch tensors
+    verbose=True
 )
+print("Encoded test file!")
 
-
-with open(train_file, "wb") as f:
-    pickle.dump(X_train_encoded, f)
-
+print("Writing to disk...")
 with open(test_file, "wb") as f:
     pickle.dump(X_test_encoded, f)
+print("Written to disk!")
