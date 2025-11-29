f6cd4ef6b4af5789c88414bfbe1312c80f549c57
Mahasvan <mahasvan@gmail.com>
2025-11-29 09:54:14 +0530

    add documentation to record loss

--- dev/early_stopper.py
+++ dev/early_stopper.py
@@ -14,14 +14,23 @@ class EarlyStopping:
 
     def record(self, value, window=3, step=1):
         """
-        :param value: the loss vaue to be recorded
+        This function records the latest loss value, and returns True if the training loop should terminate.
+
+        If `step` is 1, `Δ[n] = history[n] - history[n-1]`.
+
+        If step is 2, `Δ[n] = history[n] - history[n-2]`.
+
+        `step` is set to 2 for dynamic batch training, to separately calculate loss derivatives for normal and dynamic epochs.
+
+        :param value: the loss value to be recorded
         :param window: the number of past loss values to consider
+        :param step: the distance from which delta is calculated.
         :return: True if early stopping, False otherwise
         """
         self.history.append(value)
         if len(self.history)  < window+1: return False
         derivative = self._get_derivative(step)[-window:]
         # print(derivative)
-        # if for three continuous epochs, the loss keeps increasing, then stop the training.
+        # if for {window} continuous epochs, the loss keeps increasing, then stop the training.
         if all([x > 0 for x in derivative]): return True
         return False
